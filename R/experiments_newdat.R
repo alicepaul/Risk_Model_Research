29193730621a4b28f511ff4b0d9c984124a03368
OscarYan0104yy <90220123+OscarYan0104yy@users.noreply.github.com>
2023-10-12 14:50:59 -0400

    Simulation_2023.10.11

--- R/experiments_newdat.R
+++ R/experiments_newdat.R
@@ -16,6 +16,12 @@ run_experiments <- function(my_path){
                         acc = numeric(), sens = numeric(), spec = numeric(),
                         sec = numeric())
   
+  results_2 <- data.frame(data = character(), n = numeric(), p = numeric(),
+                        lambda0 = numeric(), non_zeros = numeric(), 
+                        med_abs = numeric(), max_abs = numeric(),
+                        acc = numeric(), sens = numeric(), spec = numeric(),
+                        sec = numeric())
+  
   # Iterate through files
   for (f in files){
     if (length(grep("_data.csv", f)) == 0) next
@@ -38,8 +44,32 @@ run_experiments <- function(my_path){
     }
     
     # Run algorithm to get risk model
+    # Testing for lambda_min as lambda 0
     t1 <- Sys.time()
     lambda0 <- cv_risk_mod(X, y, weights=weights, nfolds = 5)$lambda_min # chenge for cv
+    #lambda1se <- cv_risk_mod(X, y, weights=weights, nfolds = 5)$lambda_1se
+    mod <- risk_mod(X, y, weights=weights, lambda0 = lambda0) # change for cv
+    t2 <- Sys.time()
+    
+    
+    # Get evaluation metrics
+    time_secs <- t2 - t1
+    res_metrics <- get_metrics(mod)
+    non_zeros <- sum(mod$beta[-1] != 0)
+    med_abs <- median(abs(mod$beta[-1]))
+    max_abs <- max(abs(mod$beta[-1]))
+    
+    # Add row to data frame
+    file_row <- data.frame(data=f, n = nrow(X), p = ncol(X), lambda0 = lambda0, 
+                           non_zeros = non_zeros, med_abs = med_abs, max_abs = max_abs,
+                           acc = res_metrics$acc, sens = res_metrics$sens, 
+                           spec = res_metrics$spec, sec = time_secs) # change for cv  
+    results <- rbind(results, file_row)
+    
+    # Testing for lambda_1se as lambda 0
+    t1 <- Sys.time()
+    lambda0 <- cv_risk_mod(X, y, weights=weights, nfolds = 5)$lambda_1se # chenge for cv
+    #lambda1se <- cv_risk_mod(X, y, weights=weights, nfolds = 5)$lambda_1se
     mod <- risk_mod(X, y, weights=weights, lambda0 = lambda0) # change for cv
     t2 <- Sys.time()
     
@@ -55,9 +85,11 @@ run_experiments <- function(my_path){
                            non_zeros = non_zeros, med_abs = med_abs, max_abs = max_abs,
                            acc = res_metrics$acc, sens = res_metrics$sens, 
                            spec = res_metrics$spec, sec = time_secs) # change for cv  
-    results <- rbind(results, file_row)
+    results_2 <- rbind(results_2, file_row)
+
   }
-  write.csv(results, paste0("/Users/oscar/Documents/GitHub/Risk_Model_Research/sim_dat_new", "ncd_cv_R_newdat.csv"), row.names=FALSE)
+  write.csv(results, paste0("/Users/oscar/Documents/GitHub/Risk_Model_Research/", "ncd_cv_R_newdat_min.csv"), row.names=FALSE)
+  write.csv(results_2, paste0("/Users/oscar/Documents/GitHub/Risk_Model_Research/", "ncd_cv_R_newdat_1se.csv"), row.names=FALSE)
 }
 
 run_experiments("/Users/oscar/Documents/GitHub/Risk_Model_Research/sim_dat_new/")
