50b1b5484c6c503a02f2dce17b2ffcd7e6429365
OscarYan0104yy <90220123+OscarYan0104yy@users.noreply.github.com>
2023-11-15 21:30:33 -0500

    ompr update 11.15

--- R/ompr copy.R
+++ R/ompr copy.R
@@ -29,25 +29,26 @@ df <- read.csv(paste0("/Users/oscar/Documents/GitHub/Risk_Model_Research/MILP/da
 
 # Set data matrix
 # works out fine for 1:5, errors when trying yo increase sample size
-df <- df[1:50,]
+df <- df[1:10,]
 y <- df[[1]]
 X <- as.matrix(df[,2:ncol(df)])
-#X[,7] <- 1
+
 
 # Warm start solution
 n = nrow(X)
 p = ncol(X)
-M = 999999999 #
+M = 1000 #
 
 Lambda0 = 0
 # Score Pool
-SK_pool = c((-5*p) : (5*p)) # Determined by lb and ub of beta
+SK_pool <- c((-5*p) : (5*p)) # Determined by lb and ub of beta
 
 # Probability pool
-PI = seq(0,1,length=100) # Equally spaced between 0,1, length = 100 
+PI <- seq(0,1,length=100) # Equally spaced between 0,1, length = 100 
 # Delete 0 and 1 so that obj can take log
 PI <- PI[-c(1,100)]
-PI[1]
+
+
 
 t1 = Sys.time()
 # Model description 
@@ -57,43 +58,40 @@ MILP_V2 <- MIPModel() %>%
   # Predicted Risk Score from the integer coefficients 
   add_variable(s[i], i= 1:n, type = 'integer') %>% 
   # Exchanged this line on 11.8.2023
-  add_constraint(sum_over(beta[j]*X[i,j], j=1:p) - s[i] == 0, i=1:n) %>%  
+  add_constraint(sum_over(beta[j]*X[i,j], j=1:p) - s[i] == 0,i=1:n) %>%  
   # Indicator of S_i = k, k: one potential score from the score pool
   add_variable(z_ik[i,k], i=1:n, k=1:length(SK_pool), type = 'binary') %>% 
   add_constraint(sum_over(z_ik[i,k], k = 1:length(SK_pool)) == 1, i=1:n) %>%
+    # old
   add_constraint(s[i] - SK_pool[k] <= M * (1-z_ik[i,k]), i=1:n,k=1:length(SK_pool)) %>% 
   add_constraint(s[i] - SK_pool[k] >= -M * (1-z_ik[i,k]), i=1:n,k=1:length(SK_pool)) %>%
+    # new
+  #add_constraint((1-z_ik[i,k]) / (s[i] - SK_pool[k]) <= 1/M , i=1:n,k=1:length(SK_pool)) %>% 
+  #add_constraint((1-z_ik[i,k]) / (s[i] - SK_pool[k]) >= -1/M, i=1:n,k=1:length(SK_pool)) %>%
   # Indicator of score k assigned to prob pi_l
   add_variable(z_kl[k,l], k=1:length(SK_pool), l=1:length(PI), type = 'binary') %>% 
-  # each k is asigened to exactly 1 probs 
-  add_constraint(sum_expr(z_kl[k,l], l=1:length(PI)) == 1,k=1:length(SK_pool)) %>% 
-  #higher k is associated with higher probs 
-  #add_constraint(z_kl[k,l] <= 1 - z_kl[k-1,l_star], l=1:length(PI),l_star=1:length(PI),k=1:length(K)) %>%
-  #add_variable(w[l_star],l_star=1:length(PI),type = 'integer') 
-
-  
-  #if (l < w[l_star]) {
-    
-  #  MILP_V2 <- MILP_V2 %>% 
-  #    add_constraint(z_kl[k,l] <= 1 - z_kl[k-1,w[l_star]], l=1:length(PI),l_star=1:length(PI),k=1:length(K))
-#  } 
-   
-#MILP_V2 <- MILP_V2 %>%
+  # each k is assigened to exactly 1 probs 
+  add_constraint(sum_over(z_kl[k,l], l=1:length(PI)) == 1,k=1:length(SK_pool)) %>% 
   # Indicator of point i assigned with prob pi_l
   add_variable(p_il[i,l], i=1:n, l=1:length(PI),type = 'binary') %>% 
-  add_constraint(p_il[i,l] >= z_kl[k,l] + z_ik[i,k] - 1, i=1:n,k=1:length(SK_pool),l=1:length(PI)) %>% 
+    # old 
+  add_constraint(p_il[i,l] >= z_kl[k,l] + z_ik[i,k] - 1, i=1:n, k=1:length(SK_pool), l=1:length(PI)) %>% 
+    # new
+  #add_constraint(p_il[i,l] - z_kl[k,l] - z_ik[i,k] >= -1, i=1:n, k=1:length(SK_pool), l=1:length(PI)) %>% 
   # Each i have exactly one probs
-  add_constraint(sum_expr(p_il[i,l], l=1:length(PI)) == 1, i=1:n) %>% 
+  add_constraint(sum_over(p_il[i,l], l=1:length(PI)) == 1, i=1:n) %>% 
   #Penalty Expression
   #add_variable(lambda[j],j=1:p,type = 'binary') %>% 
   #add_constraint(beta[j] <= M*lambda[j], j=1:p) %>% 
   #add_constraint(beta[j] <= -M*lambda[j], j=1:p) %>% 
   # Objective Function
-  set_objective( - (sum_over(y[i] * log(PI[l]) * p_il[i,l] + (1-y[i]) * log(1-PI[l]) * p_il[i,l], i=1:n,l=1:length(PI))), sense = "min") %>% 
+  set_objective(  - (sum_over(y[i] * log(PI[l]) * p_il[i,l] + (1-y[i]) * log(1-PI[l]) * p_il[i,l],i=1:n,l=1:length(PI)))
+                 ,sense = "min") %>% 
   solve_model(with_ROI(solver = "glpk"))
 
 # Lambda in obj
-# + Lambda0 * sum_expr(lambda[j],j=1:p)
+# 
+
 t2 = Sys.time()
 
 time.diff <- t2 - t1
@@ -102,12 +100,15 @@ time.diff <- t2 - t1
 beta <- MILP_V2 %>% get_solution(beta[j])
 s <- MILP_V2 %>% get_solution(s[i])
 
+
 t(as.matrix(beta[,3])) %*% as.matrix(X[1,])
 t(as.matrix(beta[,3])) %*% as.matrix(X[2,])
 t(as.matrix(beta[,3])) %*% as.matrix(X[3,])
+#t(as.matrix(beta[,3])) %*% as.matrix(X[7,])
 
 
-# 
+
+#
 zik <- MILP_V2 %>% get_solution(z_ik[i,k])
 zik %>% filter(value==1)
 zik %>% group_by(i) %>% summarise(w <- sum(value))
@@ -117,6 +118,7 @@ zkl %>% group_by(k) %>% summarise(w <- sum(value))
 zkl %>% filter(value==1)
 # 
 pil <- MILP_V2 %>% get_solution(p_il[i,l]) 
+pil %>% group_by(i) %>% summarise(sum_p = sum(value))
 pil %>% filter(value==1)
 
 # Lambda
@@ -125,12 +127,10 @@ pil %>% filter(value==1)
 # Only works when there's a column with all being 1. then that j is -50 others are 0
 MILP_V2$objective_value
 
-# Based on the MILP, Beta = 0,0,-50,0,0,0,0,0,0,0 ; S = -50,-50,-50,-50,-50 ; optimal k <- 1, l <- 59
-- (log(PI[40]) * sum(y==1) + log(1-PI[40]) * sum(y!=1))
 
 
 # Results from risk.mod
-df <- df[1:5,]
+df <- df[1:10,1:5]
 y <- df[[1]]
 X <- as.matrix(df[,2:ncol(df)])
 X <- cbind(rep(1,nrow(X)), X) 
